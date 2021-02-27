3363def8c07955357b27fae5c124e1f739d1e386
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-27 15:18:41 -0600

    first commit

---
+++ pages/User_services_menu_buttons/index.js
@@ -0,0 +1,12 @@
+
+function UserServicesMenuButtons(){
+    return(
+        <div>
+            <button>Reservaciones</button>
+            <button>Anteriores</button>
+            <button>Favoritos</button>
+        </div>
+    );
+}
+
+export default UserServicesMenuButtons
