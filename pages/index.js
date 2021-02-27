0eb0ca09bc6e9541fe14e83fe93259b2b4b3725f
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-27 15:30:08 -0600

    align of the main container and folders for components and styles added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import UserServicesMenuButtons from './User_services_menu_buttons'
+import UserServicesMenuButtons from './User_services_tab/User_services_menu_buttons'
 export default function Home() {
   return (
     <div className={styles.container}>
