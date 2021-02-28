bf3e6e08ea5ec9ce5e0fc12d16f494fb0887d5d5
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-27 18:19:59 -0600

    styles of the tab buttons for services

--- pages/User_services_tab/User_services_menu_buttons/index.js
+++ pages/User_services_tab/User_services_menu_buttons/index.js
@@ -1,10 +1,10 @@
-
+import styles from '../../../styles/UserServices.module.css'
 function UserServicesMenuButtons(){
     return(
-        <div>
-            <button>Reservaciones</button>
-            <button>Anteriores</button>
-            <button>Favoritos</button>
+        <div className={styles.TabButtonsContainer}>
+            <button className={styles.TabButtons}>Reservaciones</button>
+            <button className={styles.TabButtons}>Anteriores</button>
+            <button className={styles.TabButtons}>Favoritos</button>
         </div>
     );
 }
