a69297f3488cbd80b1e7c0a9b95a544ecc865151
moctezumadaniel <moctezumadanielt@gmail.com>
2021-03-01 16:30:20 -0600

    tab button of the customer profile added with initial styles

--- pages/Customer_profile_template/TabButtons.js
+++ pages/Customer_profile_template/TabButtons.js
@@ -1,1 +1,16 @@
-/*no changes*/ 
+import styles from '../../styles/CustomerProfileTemplate.module.css'
+
+function TabButtons (){
+    const servicesTabDescription = "Servicios";
+    const reviewsTabDescription = "Reseñas";
+    const reservationsTabDescription = "Reservaciones"
+    return(
+        <div className={styles.TabButtonsContainer}>
+            <button className={styles.TabButtonActive}>{servicesTabDescription}</button>
+            <button className={styles.TabButton}>{reviewsTabDescription}</button>
+            <button className={styles.TabButton}>{reservationsTabDescription}</button>
+        </div>
+    )
+} 
+
+export default TabButtons
--- pages/Customer_profile_template/index.js
+++ pages/Customer_profile_template/index.js
@@ -1,10 +1,13 @@
 import TopBar from './Top_description'
 import MoreInformation from './MoreInformation'
+import TabButtons from './TabButtons'
 function CustomerProfileTemplate (){
     return(
         <div>
             <TopBar/>
             <MoreInformation/>
+            <TabButtons/>
+
         </div>
     )
 }
