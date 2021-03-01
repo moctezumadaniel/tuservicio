f7c7cd75107868cd340ed78671dbc2bb4883a4fc
Daniel Moctezuma <moctezumadanielt@gmail.com>
2021-02-28 19:49:27 -0600

    item super simplified added and main buttons fixed display

---
+++ pages/User_service_preview/index.js
@@ -0,0 +1,13 @@
+function UserServicePreview(){
+    return(
+        <div>
+            <image>IMAGENES DEL SERVICIO</image>
+            <div>Nombre del servicio</div>
+            <div>Puntuacion del servicio</div>
+            <div>Horario (abierto o no)</div>
+            <div>Descripcion del servicio</div>
+        </div>
+    )
+}
+
+export default UserServicePreview
--- pages/User_services_tab/index.js
+++ pages/User_services_tab/index.js
@@ -1,10 +1,12 @@
 import UserServicesTabButtons from './UserServicesTabButtons'
 import UserSearch from './UserSearch'
+import UserServicePreview from '../User_service_preview';
 function UserServicesTab(){
     return(
         <div>
             <UserSearch/>
            <UserServicesTabButtons/>
+           <UserServicePreview/>
         </div>
     );
 }
