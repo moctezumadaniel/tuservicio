9253dc054c0d6dccdff33343a8601aa328096ac8
moctezumadaniel <moctezumadanielt@gmail.com>
2021-03-02 13:24:30 -0600

    wrap added to services container and more items added just as and example

--- pages/Customer_profile_template/ServicesTab.js
+++ pages/Customer_profile_template/ServicesTab.js
@@ -1,7 +1,20 @@
+import styles from '../../styles/CustomerProfileTemplate.module.css'
+
 import ServiceItemTemplate from './ServiceItemTemplate'
 function ServicesTab (){
     return(
-        <div>
+        <div className={styles.ServicesMainContainer}>
+            <ServiceItemTemplate/>
+            <ServiceItemTemplate/>
+            <ServiceItemTemplate/>
+            <ServiceItemTemplate/>
+            <ServiceItemTemplate/>
+            <ServiceItemTemplate/>
+            <ServiceItemTemplate/>
+            <ServiceItemTemplate/>
+            <ServiceItemTemplate/>
+            <ServiceItemTemplate/>
+            <ServiceItemTemplate/>
             <ServiceItemTemplate/>
         </div>
     )
