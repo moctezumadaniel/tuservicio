795c616e3c53ccbe0b939a6d4540daf499c01c4a
moctezumadaniel <moctezumadanielt@gmail.com>
2021-03-04 11:00:45 -0600

    add review button added to the tab buttns in the custumer profile

--- pages/Customer_profile_template/TabButtons.js
+++ pages/Customer_profile_template/TabButtons.js
@@ -4,11 +4,17 @@ function TabButtons (){
     const servicesTabDescription = "Servicios";
     const reviewsTabDescription = "Reseñas";
     const reservationsTabDescription = "Reservaciones"
+    const addReviewButtonDescription = "AÑADIR RESEÑA" /*MODIFICAR MI RESENA (Cuando detecte que esta añadida)*/ 
     return(
-        <div className={styles.TabButtonsContainer}>
-            <button className={styles.TabButtonActive}>{servicesTabDescription}</button>
-            <button className={styles.TabButton}>{reviewsTabDescription}</button>
-            <button className={styles.TabButton}>{reservationsTabDescription}</button>
+        <div>
+            <div className={styles.TabButtonsContainer}>
+                <button className={styles.TabButton}>{servicesTabDescription}</button>
+                <button className={styles.TabButtonActive}>{reviewsTabDescription}</button>
+                <button className={styles.TabButton}>{reservationsTabDescription}</button>
+            </div>
+            <div className={styles.AddReviewContainer}>
+                <button className={styles.AddReviewButton}>{addReviewButtonDescription}</button>
+            </div>
         </div>
     )
 } 
