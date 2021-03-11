f054aba9cc19a85ebbaea0c823c640743d47f88a
moctezumadaniel <moctezumadanielt@gmail.com>
2021-03-11 11:50:12 -0600

    logic and styles to change the main tab message button to be able to close the current chat

--- components/Main_tab_buttons/index.js
+++ components/Main_tab_buttons/index.js
@@ -1,8 +1,11 @@
 import styles from '../../styles/MainTabButtons.module.css'
 import {useSelector, useDispatch} from 'react-redux'
 import {changeGlobalToServices, changeGlobalToPreferences, changeGlobalToMessages} from '../../redux/actions/GlobalTab'
+import {changeMessagesTabToList} from '../../redux/actions/UserMessagesTab'
 function MainTabButtons (){
     const currentTab = useSelector(state=>state.globalTab);
+    const messagesStatus = useSelector(state=>state.userMessagesTab)
+    const closeChatDescription = "< SALIR"
     const dispatch = useDispatch();
 
     function handleServicesPress(){
@@ -14,6 +17,10 @@ function MainTabButtons (){
     function handleMessagesPress(){
         dispatch(changeGlobalToMessages())
     }
+    function handleCloseChatPress(){
+        dispatch(changeMessagesTabToList())
+    }
+
     return(
         <div className={styles.MainTabButtonsContainer}>
 
@@ -22,10 +29,18 @@ function MainTabButtons (){
                 Servicios
             </button>
 
+            {
+            (messagesStatus === 'Chat' && currentTab === 'Messages')?
+            <button className={styles.MainCloseChat}
+            onClick={handleCloseChatPress}>
+                {closeChatDescription}
+            </button>
+            :
             <button className={currentTab === 'Messages' ? styles.MainTabButtonActive : styles.MainTabButton}
             onClick={handleMessagesPress}>
                 Mensajes
             </button>
+            }
 
             <button className={currentTab === 'Preferences' ? styles.MainTabButtonActive : styles.MainTabButton}
             onClick={handlePreferencesPress}>
