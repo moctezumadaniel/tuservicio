383396f2c583eb5d6d8530b5595958021e10ae91
moctezumadaniel <moctezumadanielt@gmail.com>
2021-03-14 17:49:45 -0600

    initial coponent for the customer tools added

--- components/Customer_service_dashboard/Service_tools/index.js
+++ components/Customer_service_dashboard/Service_tools/index.js
@@ -1,7 +1,37 @@
+import styles from '../../../styles/CustomerServiceDashboard.module.css'
 function ServiceTools (){
+    const orders = 'ORDENES';
+    const calculator = 'CALCULADORA';
+    const tickets = 'NOTAS DE VENTA';
+    const expenses = 'GASTOS';
+    const customersAndProviders = 'CLIENTES Y PROVEEDORES';
+    const charge = 'COBRAR';
     return(
-        <div>
-            ESTA ES LA PANTALLA DE HERRAMIENTAS
+        <div className={styles.ToolBarContainer}>
+            <div className={styles.ToolItem}>
+                <div className={styles.ToolIcon}></div>
+                <div className={styles.ToolDescription}>{orders}</div>
+            </div>
+            <div className={styles.ToolItem}>
+                <div className={styles.ToolIcon}></div>
+                <div className={styles.ToolDescription}>{calculator}</div>
+            </div>
+            <div className={styles.ToolItem}>
+                <div className={styles.ToolIcon}></div>
+                <div className={styles.ToolDescription}>{tickets}</div>
+            </div>
+            <div className={styles.ToolItem}>
+                <div className={styles.ToolIcon}></div>
+                <div className={styles.ToolDescription}>{expenses}</div>
+            </div>
+            <div className={styles.ToolItem}>
+                <div className={styles.ToolIcon}></div>
+                <div className={styles.ToolDescription}>{customersAndProviders}</div>
+            </div>
+            <div className={styles.ToolItem}>
+                <div className={styles.ToolIcon}></div>
+                <div className={styles.ToolDescription}>{charge}</div>
+            </div>
         </div>
     )
 }
