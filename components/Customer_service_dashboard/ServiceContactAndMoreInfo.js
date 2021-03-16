a94091f30f347868b53746e787c72af78793051a
moctezumadaniel <moctezumadanielt@gmail.com>
2021-03-16 17:11:22 -0600

    profile styles changed to give more space to every one of the items

--- components/Customer_service_dashboard/ServiceContactAndMoreInfo.js
+++ components/Customer_service_dashboard/ServiceContactAndMoreInfo.js
@@ -1,21 +1,46 @@
 import styles from '../../styles/CustomerServiceDashboard.module.css'
 function ServiceContactAndMoreInfo (){
-    const contactAndMoreTitle = 'Contacto y mas detalles'
+    const contactAndMoreTitle = 'Contacto y más detalles'
+    const phoneNumberTitle = 'Número de telefono'
     const phoneNumberDescription = '7712345678'
+    const addressTitle = 'Dirección del negocio'
     const addressDescription = 'Calle 00 número 0, colonia Ejemplo, Ciudad Ejemplo, Estado de Ejemplo CP 00000'
+    const moreInfoTitle = 'Mas información'
     const moreInfoReduced = 'Este es un ejemplo del inicio de una descripcion larga que detalle el negocio, este resumen se cortara despues de 140 caracteres'
+    const imagesTitle = 'Imágenes'
     const images = 'IMAGENES DEL SERVICIO, QUE DEBEN SER PREFERENTEMENTE DIFERENTES A LAS DE LOS SERVICIOS'
+    const editItemButton = 'Cambiar'
     return(
         
             <div className={styles.ContactAndMoreContainer}>
                 <div className={styles.ContactAndMoreTitle}>{contactAndMoreTitle}</div>
 
                 <div className={styles.ContactAndMoreContent}>
-                    <div className={styles.PhoneNumber}>{phoneNumberDescription}</div>
-                    <div className={styles.Address}>{addressDescription}</div>
-                    <div className={styles.MoreInfo}>{moreInfoReduced}</div>
-                    <div>{images}</div>
+                    <div className={styles.ImageNameServiceContainer}>
+                        <div className={styles.ContactAndMoreItemTitle}>{phoneNumberTitle}</div>
+                        <div className={styles.PhoneNumber}>{phoneNumberDescription}</div>
+                    </div>
+                    <button className={styles.editItemButton}>{editItemButton}</button>
                 </div>
+
+                <div className={styles.ContactAndMoreContent}>
+                    <div className={styles.ContactAndMoreItemTitle}>{addressTitle}</div>
+                    <div className={styles.Address}>{addressDescription}</div>
+                    <button className={styles.editItemButton}>{editItemButton}</button>
+                </div>
+
+                <div className={styles.ContactAndMoreContent}>
+                     <div className={styles.ContactAndMoreItemTitle}>{moreInfoTitle}</div>
+                    <div className={styles.MoreInfo}>{moreInfoReduced}</div>
+                    <button className={styles.editItemButton}>{editItemButton}</button>
+                </div>
+
+                <div className={styles.ContactAndMoreContent}>
+                    <div className={styles.ContactAndMoreItemTitle}>{imagesTitle}</div>
+                    <div>{images}</div>
+                    <button className={styles.editItemButton}>{editItemButton}</button>
+                </div>
+                
 
             </div>
     
--- components/Customer_service_dashboard/ServiceInformation.js
+++ components/Customer_service_dashboard/ServiceInformation.js
@@ -1,23 +1,38 @@
 import styles from '../../styles/CustomerServiceDashboard.module.css'
 function ServiceInformation (){
     const title = "Información"
+    const serviceImageAndNameTitle = 'Imagen y nombre'
     const serviceImage = "IMAGEN DE PERFIL"
     const serviceName = "Nombre del servicio"
+    const scheduleTitle = 'Horario de atención'
     const serviceSchedule = "8:00 a 16:00"
+    const descriptionTitle = 'Descripción corta'
     const serviceDescription = "Descripcion corta del servicio Descripcion corta del servicio Descripcion corta del servicio Descripcion corta del servicio Descripcion corta del ServiceInformation"
+    const editItemButton = 'Cambiar'
     return(
-        
-            <div className={styles.ServiceInformationContainer}>
-                <div className={styles.informationTitle}>{title}</div>
-                <div className={styles.InformationContainer}>
-                    <div className={styles.ImageInformation}>{serviceImage}</div>
-                    <div className={styles.Information}>
-                        <div className={styles.ServiceName}>{serviceName}</div>
-                        <div className={styles.Schedule}>{serviceSchedule}</div>
-                        <div>{serviceDescription}</div>
-                    </div>
-                </div>
+        <div className={styles.ServiceInformationContainer}>
+            <div className={styles.informationTitle}>{title}</div>
+
+            <div className={styles.InformationContainer}>
+                <div className={styles.InformationItemTitle}>{serviceImageAndNameTitle}</div>
+                <div className={styles.ImageInformation}>{serviceImage}</div>
+                <div>{serviceName}</div>
+                <button className={styles.editItemButton}>{editItemButton}</button>
             </div>
+
+            <div className={styles.InformationContainer}>
+                    <div className={styles.InformationItemTitle}>{scheduleTitle}</div>
+                    <div className={styles.Schedule}>{serviceSchedule}</div>
+                    <button className={styles.editItemButton}>{editItemButton}</button>
+            </div>
+
+            <div className={styles.InformationContainer}>
+                    <div className={styles.InformationItemTitle}>{descriptionTitle}</div>
+                    <div>{serviceDescription}</div>
+                    <button className={styles.editItemButton}>{editItemButton}</button>
+            </div>
+            
+        </div>
         
 
     )
