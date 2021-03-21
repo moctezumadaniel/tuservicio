7ac3e2c5a83760fc37a3a6126d05599d6e955694
moctezumadaniel <moctezumadanielt@gmail.com>
2021-03-20 19:55:53 -0600

    styles of the dashboard expenses tool improved

--- components/ExpensesTool.js/index.js
+++ components/ExpensesTool.js/index.js
@@ -3,8 +3,9 @@ function ExpensesTool (){
     const registerExpenseButton = '+ Registrar nuevo gasto'
     const customExpensesRange = 'Seleccionar periodo personalizado'
     const dateRangeSelected = 'Marzo 2021'
-    const dateRangeTotalDescription = 'Gastos totales'
+    const dateRangeTotalDescription = 'Gastos totales:'
     const dateRangeTotal = '$25,000'
+    const shareReportButton = 'Descargar / Compartir reporte de gastos'
     const expenseItemDate = '23 de Marzo'
     const expenseItemAmounth = '$12,500'
     const expenseItemDescription = 'Ejemplo de descripcion del servicio Ejemplo de descripcion del servicio Ejemplo de descripcion del servicio'
@@ -14,19 +15,65 @@ function ExpensesTool (){
             <button className={styles.RegisterExpenseButton}>{registerExpenseButton}</button>
             <button className={styles.CustomDateRangeButton}>{customExpensesRange}</button>
             <div className={styles.DateRangeSelected}>{dateRangeSelected}</div>
+            
             <div className={styles.GrandTotalContainer}>
-                <div>{dateRangeTotalDescription}</div>
-                <div>{dateRangeTotal}</div>
+                <div className={styles.GrandTotalDescription}>{dateRangeTotalDescription}</div>
+                <div className={styles.GrandTotal}>{dateRangeTotal}</div>
             </div>
 
-            <div className={styles.ExpenseItemContainer}>
-                <div className={styles.ExpenseMainInformation}>
-                    <div>{expenseItemDate}</div>
-                    <div>{expenseItemAmounth}</div>
+            <button className={styles.ShareReportButton}>{shareReportButton}</button>
+            
+{/*LIST OF EXPENSES*/}
+            <div className={styles.ListOfExpensesContainer}>
+              
+                <div className={styles.ExpenseItemContainer}>
+                    <div className={styles.ExpenseMainInformation}>
+                        <div>{expenseItemDate}</div>
+                        <div>{expenseItemAmounth}</div>
+                    </div>
+                    <div>{expenseItemDescription}</div>
+                    <button className={styles.OpenEditItemButton}>{openEditItemButton}</button>
                 </div>
-                <div>{expenseItemDescription}</div>
-                <button>{openEditItemButton}</button>
+
+                <div className={styles.ExpenseItemContainer}>
+                    <div className={styles.ExpenseMainInformation}>
+                        <div>{expenseItemDate}</div>
+                        <div>{expenseItemAmounth}</div>
+                    </div>
+                    <div>{expenseItemDescription}</div>
+                    <button className={styles.OpenEditItemButton}>{openEditItemButton}</button>
+                </div>
+
+                <div className={styles.ExpenseItemContainer}>
+                    <div className={styles.ExpenseMainInformation}>
+                        <div>{expenseItemDate}</div>
+                        <div>{expenseItemAmounth}</div>
+                    </div>
+                    <div>{expenseItemDescription}</div>
+                    <button className={styles.OpenEditItemButton}>{openEditItemButton}</button>
+                </div>
+
+                <div className={styles.ExpenseItemContainer}>
+                    <div className={styles.ExpenseMainInformation}>
+                        <div>{expenseItemDate}</div>
+                        <div>{expenseItemAmounth}</div>
+                    </div>
+                    <div>{expenseItemDescription}</div>
+                    <button className={styles.OpenEditItemButton}>{openEditItemButton}</button>
+                </div>
+
+                <div className={styles.ExpenseItemContainer}>
+                    <div className={styles.ExpenseMainInformation}>
+                        <div>{expenseItemDate}</div>
+                        <div>{expenseItemAmounth}</div>
+                    </div>
+                    <div>{expenseItemDescription}</div>
+                    <button className={styles.OpenEditItemButton}>{openEditItemButton}</button>
+                </div>
+                
             </div>
+            
+
         </div>
     )
 }
