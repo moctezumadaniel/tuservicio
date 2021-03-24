895a2365c49b4be400a47330651306b3f578be97
moctezumadaniel <moctezumadanielt@gmail.com>
2021-03-23 19:42:13 -0600

    credit and payment component for the customers tool

--- components/CustomersTool/CustomerCredit.js
+++ components/CustomersTool/CustomerCredit.js
@@ -1,10 +1,23 @@
+import styles from '../../styles/CustomersTool.module.css'
 function CustomerCredit (){
+    const nameInput = 'Escribe el nombre del cliente';
+    const amounthInput = 'Escribe el monto del crédito'
+    const save = 'GUARDAR CRÉDITO';
+    const share = 'COMPARTIR / GUARDAR'
     return(
-        <div>
-            <input type='text'/>
-            <input type='date'/>
-            <input type='number'/>
-            <textarea/>
+        <div className={styles.CreditFormContainer}>
+            <input type='text' className={styles.CustomerNameInput}
+            placeholder={nameInput}/>
+            <div className={styles.SecondaryInputsContainer}>
+                <input type='date' className={styles.DateInput}/>
+                <input type='number' className={styles.AmounthInput}
+                placeholder={amounthInput}/>
+            </div>
+            <div className={styles.SecondaryButtonsContainer}>
+                <button className={styles.SaveCreditButton}>{save}</button>
+                <button className={styles.ShareButton}>{share}</button>
+            </div>
+            <textarea className={styles.TextAreaForm}/>
         </div>
     )
 }
--- components/CustomersTool/CustomerPayment.js
+++ components/CustomersTool/CustomerPayment.js
@@ -1,7 +1,23 @@
+import styles from '../../styles/CustomersTool.module.css'
 function CustomerPayment (){
+    const nameInput = 'Escribe el nombre del cliente';
+    const amounthInput = 'Escribe el monto del crédito'
+    const save = 'GUARDAR CRÉDITO';
+    const share = 'COMPARTIR / GUARDAR'
     return(
-        <div>
-            ESTE ES EL COMPONENTE PARA REGISTRAR LOS PAGOS DE LOS CLIENTES
+        <div className={styles.CreditFormContainer}>
+            <input type='text' className={styles.CustomerNameInput}
+            placeholder={nameInput}/>
+            <div className={styles.SecondaryInputsContainer}>
+                <input type='date' className={styles.DateInput}/>
+                <input type='number' className={styles.AmounthInput}
+                placeholder={amounthInput}/>
+            </div>
+            <div className={styles.SecondaryButtonsContainer}>
+                <button className={styles.SavePaymentButton}>{save}</button>
+                <button className={styles.ShareButton}>{share}</button>
+            </div>
+            <textarea className={styles.TextAreaForm}/>
         </div>
     )
 }
