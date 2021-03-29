6df95a667dadaff9c92a0bf31dd2440e51b48cd2
moctezumadaniel <moctezumadanielt@gmail.com>
2021-03-29 13:25:48 -0600

    new buttons and components to complement the ticket form

--- components/TicketsTool/TicketsForm.js
+++ components/TicketsTool/TicketsForm.js
@@ -3,23 +3,31 @@ function TicketForm(){
     const ticketTitle = 'Nota '
     const ticketNumber = '38-19032021'
     const newItemTitle = 'Nuevo concepto'
+    const deleteItemButton = 'Eliminar'
     const newDescriptionPlaceholder = 'Escribe la descripción del nuevo concepto';
-    const newAmounthPlaceholder = 'Escribe el monto del nuevo concepto'
+    const newAmounthPlaceholder = 'Escribe el monto del concepto'
     const addConceptButton = 'AGREGAR'
+    const namePlaceholder = 'Escribe el nombre del cliente'
+    const grandTotalTitle = 'Total'
+    const grandTotal = '$25,000'
     return(
         <div className={styles.TicketsToolMainContainer}>
             <div className={styles.TicketTitle}>
                 {ticketTitle}{ticketNumber}
             </div>
+{/*DATE AND NAME OF THE CUSTOMER */}
+            <div className={styles.TicketDateCustomerContainer}>
+                <input type='date' className={styles.TicketDateInput}/>
+                <input type='text' className={styles.TicketNameInput}
+                placeholder={namePlaceholder}/>
+            </div>
 {/*LIST OF ITEMS ADDED */}
             <div className={styles.TicketItemContainer}>
                 <div className={styles.TicketDescription}>Nueva descripcion del servicio</div>
-                <div className={styles.TicketAmounth}>120,000,000,000.24</div>
-            </div>
-
-            <div className={styles.TicketItemContainer}>
-                <div className={styles.TicketDescription}>Esta es otra descripcion del servicio</div>
-                <div className={styles.TicketAmounth}>12,000</div>
+                <div className={styles.ItemAmounthDescription}>
+                    <div className={styles.TicketAmounth}>120,000,000,000.24</div>
+                    <button className={styles.DeleteItemButton}>{deleteItemButton}</button>
+                </div>
             </div>
 
 {/*NEW ITEM FORM */}
@@ -30,6 +38,12 @@ function TicketForm(){
                 <input type='number' className={styles.InputTicketAmounth}
                 placeholder={newAmounthPlaceholder}/>
                 <button className={styles.AddItemButton}>{addConceptButton}</button>
+            </div>
+            
+{/*TICKET GRAND TOTAL*/}
+<div className={styles.GrandTotalContainer}>
+                <div className={styles.GrandTotalTitle}>{grandTotalTitle}</div>
+                <div className={styles.GrandTotal}>{grandTotal}</div>
             </div>
 
         </div>
