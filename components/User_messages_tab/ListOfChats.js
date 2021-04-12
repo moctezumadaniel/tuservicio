aba2a276a16b40e385f8a091e2c9370f723f92dc
moctezumadaniel <moctezumadanielt@gmail.com>
2021-04-11 19:32:55 -0500

    improved styles for the messages

--- components/User_messages_tab/ListOfChats.js
+++ components/User_messages_tab/ListOfChats.js
@@ -1,22 +1,29 @@
 import styles from '../../styles/UserMessages.module.css'
 import ChatProfilePreview from './ChatProfilePreview'
 function ListOfChats (){
+    const chatSearchPlaceholder = "Escribe el nombre del servicio"/*CHANGE BETWEEN CUSTOMER AND USER */
     return(
-        <div className={styles.ListOfChatsContainer}>
-            <ChatProfilePreview/>
-            <ChatProfilePreview/>
-            <ChatProfilePreview/>
-            <ChatProfilePreview/>
-            <ChatProfilePreview/>
-            <ChatProfilePreview/>
-            <ChatProfilePreview/>
-            <ChatProfilePreview/>
-            <ChatProfilePreview/>
-            <ChatProfilePreview/>
-            <ChatProfilePreview/>
-            <ChatProfilePreview/>
-            <ChatProfilePreview/>
-            <ChatProfilePreview/>
+        <div>
+            <div className={styles.ChatSearchContainer}>
+                <input type="search" className={styles.ChatSearch}
+                placeholder={chatSearchPlaceholder}/>
+            </div>
+            <div className={styles.ListOfChatsContainer}>
+                <ChatProfilePreview/>
+                <ChatProfilePreview/>
+                <ChatProfilePreview/>
+                <ChatProfilePreview/>
+                <ChatProfilePreview/>
+                <ChatProfilePreview/>
+                <ChatProfilePreview/>
+                <ChatProfilePreview/>
+                <ChatProfilePreview/>
+                <ChatProfilePreview/>
+                <ChatProfilePreview/>
+                <ChatProfilePreview/>
+                <ChatProfilePreview/>
+                <ChatProfilePreview/>
+            </div>
         </div>
     )
 }
