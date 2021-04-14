95a4bc93a70dbdd1baa4153012891dac6520c48e
moctezumadaniel <moctezumadanielt@gmail.com>
2021-04-14 18:49:18 -0500

    final searchbar styles

--- components/User_services_tab/UserSearch.js
+++ components/User_services_tab/UserSearch.js
@@ -4,15 +4,17 @@ import {changeToSearch} from '../../redux/actions/UserServicesTab'
 function UserSearch(){
     const dispatch = useDispatch()
     const placeholder = "Escribe el servicio que necesites"
-    const serachButton = "Buscar"
+    const searchButton = "Buscar"
     function handleSearchButton (){
         dispatch(changeToSearch())
     }
     return(
-        <div className={styles.SearchBarContainer}>
-            <input className={styles.SearchBar}type="search" placeholder={placeholder}/>
-            <button className={styles.SearchButton}
-            onClick={handleSearchButton}>{serachButton}</button>
+        <div className={styles.SearchBarMainContainer}>
+            <div className={styles.SearchBarContainer}>
+                <input className={styles.SearchBar}type="search" placeholder={placeholder}/>
+                <button className={styles.SearchButton}
+                onClick={handleSearchButton}>{searchButton}</button>
+            </div>
         </div>
     )
 }
