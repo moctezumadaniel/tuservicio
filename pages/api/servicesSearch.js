488bc11850630777b9fbae5d1a81cec3d3f356d2
moctezumadaniel <moctezumadanielt@gmail.com>
2021-05-14 23:03:54 -0500

    conflictive api deleted

--- pages/api/servicesSearch.js
+++
@@ -1,9 +0,0 @@
-import {connectToDatabase } from '../../middleware/mongodb';
-
-export default async function serviceSearch (req, res){
-    const {db} = await connectToDatabase();
-    
-    const data = await db.collection("customers_public_data").find({}).limit(20).toArray()
-
-    res.json(data)
-}
