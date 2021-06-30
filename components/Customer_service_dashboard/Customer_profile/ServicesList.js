5cc4a37f9a71a8faabda65e326e5f65157d36bb0
moctezumadaniel <moctezumadanielt@gmail.com>
2021-06-30 14:40:54 -0500

    buttons to edit and delete service added without function and redering of the list of services

--- components/Customer_service_dashboard/Customer_profile/ServicesList.js
+++ components/Customer_service_dashboard/Customer_profile/ServicesList.js
@@ -1,14 +1,43 @@
 import styles from '../../../styles/CustomerServiceDashboard.module.css'
-import {useDispatch} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import {changeCustomerProfileFormToAddService} from '../../../redux/actions/CustomerProfileForms'
-function ServicesList(){
-    const dispatch = useDispatch()
-    const servicesTitle = "Servicios";
-    const addServiceButton = "AÑADIR SERVICIO"
+
+function ListOfServices(){
+    const customerPublicServices = useSelector(state => state.customerPublicInformation.listOfServices)
     const image = "Imagen del servicio";{/*imagen del servicio en caso de que no se establezca una para el servicio */}
     const price = "Precio del servicio";
     const title = "Titulo del servicio";
     const description = "Este es un ejemplo de la descripcion corta de un servicio que no deberá exedor de 140 caracteres"
+    const deleteButton = "ELIMINAR"
+    const editButton = "EDITAR"
+    console.log(customerPublicServices)
+    return(
+        <div className={styles.ServicesMainContainer}>
+        {/*SERICE ITEM */}
+            {customerPublicServices.map(service => (
+            <div className={styles.ServiceItemContainer} key={service._id}>
+                <div className={styles.ServiceMainInfo}>
+                    <image>{service.image}</image>
+                    <div>{service.price}</div>
+                </div>
+                <div className={styles.ServiceComplementInfo}>
+                    <div className={styles.ServiceTitle}>{service.title}</div>
+                    <div>{service.description}</div>
+                </div>
+                <div className={styles.ServiceButtonsContainer}>
+                    <button className={styles.EditServiceButton}>{editButton}</button>
+                    <button className={styles.DeleteServiceButton}>{deleteButton}</button>
+                </div>
+            </div>
+            ))
+            }
+        </div>
+    )
+}
+function ServicesList(){
+    const dispatch = useDispatch()
+    const servicesTitle = "Servicios";
+    const addServiceButton = "AÑADIR SERVICIO"
     
     function handleAddServicePress(){
         dispatch(changeCustomerProfileFormToAddService())
@@ -20,51 +49,7 @@ function ServicesList(){
             <div className={styles.ServicesTitle}>{servicesTitle}</div>
             <button className={styles.AddServiceButton}
             onClick={handleAddServicePress}>{addServiceButton}</button>
-
-{/*LIST OF SERVICES */}
-            <div className={styles.ServicesMainContainer}>
-            {/*SERICE ITEM */}
-            <div className={styles.ServiceItemContainer}>
-                    <div className={styles.ServiceMainInfo}>
-                        <image>{image}</image>
-                            <div>{price}</div>
-                        </div>
-                            <div className={styles.ServiceComplementInfo}>
-                            <div className={styles.ServiceTitle}>{title}</div>
-                            <div>{description}</div>
-                        </div>
-            </div>
-            <div className={styles.ServiceItemContainer}>
-                    <div className={styles.ServiceMainInfo}>
-                        <image>{image}</image>
-                            <div>{price}</div>
-                        </div>
-                            <div className={styles.ServiceComplementInfo}>
-                            <div>{title}</div>
-                            <div>{description}</div>
-                        </div>
-            </div>
-            <div className={styles.ServiceItemContainer}>
-                    <div className={styles.ServiceMainInfo}>
-                        <image>{image}</image>
-                            <div>{price}</div>
-                        </div>
-                            <div className={styles.ServiceComplementInfo}>
-                            <div>{title}</div>
-                            <div>{description}</div>
-                        </div>
-            </div>
-            <div className={styles.ServiceItemContainer}>
-                    <div className={styles.ServiceMainInfo}>
-                        <image>{image}</image>
-                            <div>{price}</div>
-                        </div>
-                            <div className={styles.ServiceComplementInfo}>
-                            <div>{title}</div>
-                            <div>{description}</div>
-                        </div>
-            </div>
-            </div>
+            <ListOfServices/>
             
             
         </div>
