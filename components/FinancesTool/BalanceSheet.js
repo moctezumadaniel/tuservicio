a2b7f26f00d410fdfc861fa33673342cb03d2f21
moctezumadaniel <moctezumadanielt@gmail.com>
2021-07-26 18:03:14 -0500

    initial income statement and balance sheet components

--- components/FinancesTool/BalanceSheet.js
+++ components/FinancesTool/BalanceSheet.js
@@ -1,12 +1,62 @@
 import styles from '../../styles/FinancesTool.module.css'
 function BalanceSheet() {
     const balanceSheetTitle = 'Balance'
+    const cashTitle = 'Efectivo y Bancos:'
+    const customersTitle = 'Clientes:'
+    const debtorsTitle = 'Deudores Diversos:'
+
+    const providersTitle = 'Proveedores:'
+    const creditorsTitle = 'Acreedores Diversos:'
+
+    const profitTitle = 'Utilidad:'
+    const previpusProfits = 'Utilidades Previas:'
+    const stockholdersEquityTitle = 'Capital Social:'
     
     return (
         <div className={styles.FinanceComponentContainer}>
             <div className={styles.Title}>{balanceSheetTitle}</div>
             <div className={styles.ChartContainer}>
-                
+
+                <div className={styles.ConceptStatementContainer}>
+                    <div className={styles.ConceptDescription}>{cashTitle}</div>
+                    <div className={styles.ConceptAmouth}>$31212121</div>
+                </div>
+
+                <div className={styles.ConceptStatementContainer}>
+                    <div className={styles.ConceptDescription}>{customersTitle}</div>
+                    <div className={styles.ConceptAmouth}>$31212121</div>
+                </div>
+
+                <div className={styles.ConceptStatementContainer}>
+                    <div className={styles.ConceptDescription}>{debtorsTitle}</div>
+                    <div className={styles.ConceptAmouth}>$31212121</div>
+                </div>
+
+                <div className={styles.ConceptStatementContainer}>
+                    <div className={styles.ConceptDescription}>{providersTitle}</div>
+                    <div className={styles.ConceptAmouth}>$31212121</div>
+                </div>
+
+                <div className={styles.ConceptStatementContainer}>
+                    <div className={styles.ConceptDescription}>{creditorsTitle}</div>
+                    <div className={styles.ConceptAmouth}>$31212121</div>
+                </div>
+
+                <div className={styles.ConceptStatementContainer}>
+                    <div className={styles.ConceptDescription}>{profitTitle}</div>
+                    <div className={styles.ConceptAmouth}>$31212121</div>
+                </div>
+
+                <div className={styles.ConceptStatementContainer}>
+                    <div className={styles.ConceptDescription}>{previpusProfits}</div>
+                    <div className={styles.ConceptAmouth}>$31212121</div>
+                </div>
+
+                <div className={styles.ConceptStatementContainer}>
+                    <div className={styles.ConceptDescription}>{stockholdersEquityTitle}</div>
+                    <div className={styles.ConceptAmouth}>$31212121</div>
+                </div>
+
             </div>
         </div>
     )
--- components/FinancesTool/IncomeStatement.js
+++ components/FinancesTool/IncomeStatement.js
@@ -1,11 +1,34 @@
 import styles from '../../styles/FinancesTool.module.css'
 function IncomeStatement() {
     const incomeStatementTitle = 'Resultados'
-    
+    const totalSalesTitle = 'Ventas:'
+    const totalExpensesTitle = 'Gastos:'
+    const totalProfitTitle = 'Utilidad:'
+    const profitPorcentageTitle = ' Porcentaje de Utilidad:'
     return (
         <div className={styles.FinanceComponentContainer}>
             <div className={styles.Title}>{incomeStatementTitle}</div>
             <div className={styles.ChartContainer}>
+
+                <div className={styles.ConceptStatementContainer}>
+                    <div className={styles.ConceptDescription}>{totalSalesTitle}</div>
+                    <div className={styles.ConceptAmouth}>$31212121</div>
+                </div>
+
+                <div className={styles.ConceptStatementContainer}>
+                    <div className={styles.ConceptDescription}>{totalExpensesTitle}</div>
+                    <div className={styles.ConceptAmouth}>$31212121</div>
+                </div>
+
+                <div className={styles.ConceptStatementContainer}>
+                    <div className={styles.ConceptDescription}>{totalProfitTitle}</div>
+                    <div className={styles.ConceptAmouth}>$31212121</div>
+                </div>
+
+                <div className={styles.ConceptStatementContainer}>
+                    <div className={styles.ConceptDescription}>{profitPorcentageTitle}</div>
+                    <div className={styles.ConceptAmouth}>31.24%</div>
+                </div>
                 
             </div>
         </div>
