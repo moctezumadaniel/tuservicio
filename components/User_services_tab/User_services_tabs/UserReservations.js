294110c34b7600fbc7d8c958a21ade542e3df8f7
moctezumadaniel <moctezumadanielt@gmail.com>
2021-08-05 18:16:23 -0500

    initial user reservations rendering with local state

--- components/User_services_tab/User_services_tabs/UserReservations.js
+++ components/User_services_tab/User_services_tabs/UserReservations.js
@@ -1,145 +1,49 @@
 import styles from '../../../styles/UserReservations.module.css'
 import {UserLogin} from '../../User_login'
 import { useAuth0 } from '@auth0/auth0-react'
-function UserReservations (){
-    
-    return(
-        <div className={styles.ListOfReservationsContainer}>
-            <ListOfUserReservations/>
-             
-        </div>
-    )
-}
-export default UserReservations
+import { useSelector } from 'react-redux'
 
-function ListOfUserReservations(){
+function UserReservations(){
     const {isAuthenticated, isLoading} = useAuth0()
-    const defaultPage = "Aún no tienes reservaciones"
-    const schedule = '21 de Marzo de 8:00am a 8:30am';
-    const customer = 'Nombre Completo del Cliente'
-    const description = 'Descripcion corta del motivo de la reservacion';
-    const configureButton = 'CANCELAR'
-    const doneButton = 'CONTACTAR'
     return(
         <>
         {!isAuthenticated && !isLoading?
         <UserLogin/>:
-        <div className={styles.ListOfReservationsContainer}>
-            {/*<div className={styles.DefaultPageTitle}>{defaultPage}</div>*/}
-            {/*RESERVATION ITEM */}
-            <div className={styles.ReservationItem}>
-                
-                <div className={styles.ItemSchedule}>{schedule}</div>
-                <div className={styles.ItemCustomerName}>{customer}</div>
-                <div className={styles.ItemDescription}>{description}</div>
-
-                <div className={styles.ReservationItemButtonsContainer}>
-                    <button className={styles.EditReservationButton}>{configureButton}</button>
-                    <button className={styles.AttendedReservationButton}>{doneButton}</button>
-                </div>
-
-            </div>
-
-            {/*RESERVATION ITEM */}
-            <div className={styles.ReservationItem}>
-                
-                <div className={styles.ItemSchedule}>{schedule}</div>
-                <div className={styles.ItemCustomerName}>{customer}</div>
-                <div className={styles.ItemDescription}>{description}</div>
-
-                <div className={styles.ReservationItemButtonsContainer}>
-                    <button className={styles.EditReservationButton}>{configureButton}</button>
-                    <button className={styles.AttendedReservationButton}>{doneButton}</button>
-                </div>
-
-            </div>
-
-            {/*RESERVATION ITEM */}
-            <div className={styles.ReservationItem}>
-                
-                <div className={styles.ItemSchedule}>{schedule}</div>
-                <div className={styles.ItemCustomerName}>{customer}</div>
-                <div className={styles.ItemDescription}>{description}</div>
-
-                <div className={styles.ReservationItemButtonsContainer}>
-                    <button className={styles.EditReservationButton}>{configureButton}</button>
-                    <button className={styles.AttendedReservationButton}>{doneButton}</button>
-                </div>
-
-            </div>
-
-
-            {/*RESERVATION ITEM */}
-            <div className={styles.ReservationItem}>
-                
-                <div className={styles.ItemSchedule}>{schedule}</div>
-                <div className={styles.ItemCustomerName}>{customer}</div>
-                <div className={styles.ItemDescription}>{description}</div>
-
-                <div className={styles.ReservationItemButtonsContainer}>
-                    <button className={styles.EditReservationButton}>{configureButton}</button>
-                    <button className={styles.AttendedReservationButton}>{doneButton}</button>
-                </div>
-
-            </div>
-
-            {/*RESERVATION ITEM */}
-            <div className={styles.ReservationItem}>
-                
-                <div className={styles.ItemSchedule}>{schedule}</div>
-                <div className={styles.ItemCustomerName}>{customer}</div>
-                <div className={styles.ItemDescription}>{description}</div>
-
-                <div className={styles.ReservationItemButtonsContainer}>
-                    <button className={styles.EditReservationButton}>{configureButton}</button>
-                    <button className={styles.AttendedReservationButton}>{doneButton}</button>
-                </div>
-
-            </div>
-
-            {/*RESERVATION ITEM */}
-            <div className={styles.ReservationItem}>
-                
-                <div className={styles.ItemSchedule}>{schedule}</div>
-                <div className={styles.ItemCustomerName}>{customer}</div>
-                <div className={styles.ItemDescription}>{description}</div>
-
-                <div className={styles.ReservationItemButtonsContainer}>
-                    <button className={styles.EditReservationButton}>{configureButton}</button>
-                    <button className={styles.AttendedReservationButton}>{doneButton}</button>
-                </div>
-
-            </div>
-
-            {/*RESERVATION ITEM */}
-            <div className={styles.ReservationItem}>
-                
-                <div className={styles.ItemSchedule}>{schedule}</div>
-                <div className={styles.ItemCustomerName}>{customer}</div>
-                <div className={styles.ItemDescription}>{description}</div>
-
-                <div className={styles.ReservationItemButtonsContainer}>
-                    <button className={styles.EditReservationButton}>{configureButton}</button>
-                    <button className={styles.AttendedReservationButton}>{doneButton}</button>
-                </div>
-
-            </div>
-
-            {/*RESERVATION ITEM */}
-            <div className={styles.ReservationItem}>
-                
-                <div className={styles.ItemSchedule}>{schedule}</div>
-                <div className={styles.ItemCustomerName}>{customer}</div>
-                <div className={styles.ItemDescription}>{description}</div>
-
-                <div className={styles.ReservationItemButtonsContainer}>
-                    <button className={styles.EditReservationButton}>{configureButton}</button>
-                    <button className={styles.AttendedReservationButton}>{doneButton}</button>
-                </div>
-            </div>
-
-        </div>
+        <ListOfReservations/>
         }
         </>
     )
+}
+
+export default UserReservations
+
+function ListOfReservations (){
+    const userReservations = useSelector(state => state.customerInformation.reservations)
+
+    const configureButton = 'CANCELAR'
+    const contactButton = 'CONTACTAR'
+
+    console.log(userReservations)
+    if(userReservations.length > 0){
+        return (
+            <div className={styles.ListOfReservationsContainer}>
+                {userReservations.map(reservation => (
+                <div className={styles.ReservationItem}>
+                
+                    <div className={styles.ItemSchedule}>{reservation.schedule}</div>
+                    <div className={styles.ItemCustomerName}>{reservation.customer}</div>
+                    <div className={styles.ItemDescription}>{reservation.description}</div>
+    
+                    <div className={styles.ReservationItemButtonsContainer}>
+                        <button className={styles.EditReservationButton}>{configureButton}</button>
+                        <button className={styles.AttendedReservationButton}>{contactButton}</button>
+                    </div>
+
+                </div>
+                ))
+
+                }
+            </div>
+        )
+    }
 }
