b5cfac0ce1b32a584a4a977defba3a078b1f0fb8
moctezumadaniel <moctezumadanielt@gmail.com>
2021-08-05 20:49:33 -0500

    initial map of user previous services

--- components/User_services_tab/User_services_tabs/UserPreviousServices.js
+++ components/User_services_tab/User_services_tabs/UserPreviousServices.js
@@ -2,115 +2,49 @@ import styles from '../../../styles/UserPreviousServices.module.css'
 import Image from 'next/image'
 import { UserLogin } from '../../User_login'
 import { useAuth0 } from '@auth0/auth0-react'
+import { useSelector } from 'react-redux'
 function UserPreviousServices(){
-    const {isAuthenticated, isLoading} = useAuth0()
-    const defaultPage = 'Aún no tienes servicios previos'
-    const image = "Imagen de perfil";
-    const name = "Nombre del servicio";
-    const score = "5 star";
-    const todaySchedule = "8:00 a 16:00" /*Green when is open at the moment of the search*/
-    const description = "Este es un ejemplo de la descripcion corta de un servicio que no deberá exedor de 140 caracteres"
-    
+    const {isAuthenticated, isLoading} = useAuth0()    
     return(
         <>
         {!isAuthenticated && !isLoading?
         <UserLogin/>:
-        <div className={styles.ServiceSearchMainContainer}>
-            {/*<div>{defaultPage}</div>*/}
-            {/*PREVIOUS SERVICE */}
-            <div className={styles.PreviewMainContainer}>
-                <div className={styles.ImageAndScoreContainer}>
-                    <Image src='/celerPerfil.png' width={80} height={80}/>
-                    <div>{score}</div>
-                </div>
-                <div className={styles.ServiceInformation}>
-                    <div className={styles.ServiceName}>{name}</div>
-                    <div className={styles.ServiceTodaySchedule}>{todaySchedule}</div>
-                    <div className={styles.ServiceDescription}>{description}</div>
-                </div>
-            </div>
-
-            {/*PREVIOUS SERVICE */}
-            <div className={styles.PreviewMainContainer}>
-                <div className={styles.ImageAndScoreContainer}>
-                    <Image src='/celerPerfil.png' width={80} height={80}/>
-                    <div>{score}</div>
-                </div>
-                <div className={styles.ServiceInformation}>
-                    <div className={styles.ServiceName}>{name}</div>
-                    <div className={styles.ServiceTodaySchedule}>{todaySchedule}</div>
-                    <div className={styles.ServiceDescription}>{description}</div>
-                </div>
-            </div>
-
-            {/*PREVIOUS SERVICE */}
-            <div className={styles.PreviewMainContainer}>
-                <div className={styles.ImageAndScoreContainer}>
-                    <Image src='/celerPerfil.png' width={80} height={80}/>
-                    <div>{score}</div>
-                </div>
-                <div className={styles.ServiceInformation}>
-                    <div className={styles.ServiceName}>{name}</div>
-                    <div className={styles.ServiceTodaySchedule}>{todaySchedule}</div>
-                    <div className={styles.ServiceDescription}>{description}</div>
-                </div>
-            </div>
-
-            {/*PREVIOUS SERVICE */}
-            <div className={styles.PreviewMainContainer}>
-                <div className={styles.ImageAndScoreContainer}>
-                    <Image src='/celerPerfil.png' width={80} height={80}/>
-                    <div>{score}</div>
-                </div>
-                <div className={styles.ServiceInformation}>
-                    <div className={styles.ServiceName}>{name}</div>
-                    <div className={styles.ServiceTodaySchedule}>{todaySchedule}</div>
-                    <div className={styles.ServiceDescription}>{description}</div>
-                </div>
-            </div>
-
-            {/*PREVIOUS SERVICE */}
-            <div className={styles.PreviewMainContainer}>
-                <div className={styles.ImageAndScoreContainer}>
-                    <Image src='/celerPerfil.png' width={80} height={80}/>
-                    <div>{score}</div>
-                </div>
-                <div className={styles.ServiceInformation}>
-                    <div className={styles.ServiceName}>{name}</div>
-                    <div className={styles.ServiceTodaySchedule}>{todaySchedule}</div>
-                    <div className={styles.ServiceDescription}>{description}</div>
-                </div>
-            </div>
-
-            {/*PREVIOUS SERVICE */}
-            <div className={styles.PreviewMainContainer}>
-                <div className={styles.ImageAndScoreContainer}>
-                    <Image src='/celerPerfil.png' width={80} height={80}/>
-                    <div>{score}</div>
-                </div>
-                <div className={styles.ServiceInformation}>
-                    <div className={styles.ServiceName}>{name}</div>
-                    <div className={styles.ServiceTodaySchedule}>{todaySchedule}</div>
-                    <div className={styles.ServiceDescription}>{description}</div>
-                </div>
-            </div>
-
-            {/*PREVIOUS SERVICE */}
-            <div className={styles.PreviewMainContainer}>
-                <div className={styles.ImageAndScoreContainer}>
-                    <Image src='/celerPerfil.png' width={80} height={80}/>
-                    <div>{score}</div>
-                </div>
-                <div className={styles.ServiceInformation}>
-                    <div className={styles.ServiceName}>{name}</div>
-                    <div className={styles.ServiceTodaySchedule}>{todaySchedule}</div>
-                    <div className={styles.ServiceDescription}>{description}</div>
-                </div>
-            </div>
-            
-        </div>
+        <ListOfPreviousServices/>
         }
         </>
     )
 }
 export default UserPreviousServices
+
+function ListOfPreviousServices (){
+    const previosServices = useSelector(state => state.userInformation.previousServices)
+    if(previosServices.length > 0){
+        return (
+            <div className={styles.ServiceSearchMainContainer}>
+                {previosServices.map(service => (
+                <div className={styles.PreviewMainContainer}>
+
+                    <div className={styles.ImageAndScoreContainer}>
+                        <Image src='/celerPerfil.png' width={80} height={80}/>
+                        <div>{score}</div>
+                    </div>
+
+                    <div className={styles.ServiceInformation}>
+                        <div className={styles.ServiceName}>{service.name}</div>
+                        <div className={styles.ServiceTodaySchedule}>{todaySchedule}</div>
+                        <div className={styles.ServiceDescription}>{description}</div>
+                    </div>
+
+                </div>
+                ))
+
+                }
+            </div>
+        )
+    }
+    else return (
+        <div className={styles.ServiceSearchMainContainer}>
+            Aún no se ha registrado ninguna asistencia a una cita
+        </div>
+    )
+}
