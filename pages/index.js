74602ce6ef75716fe7d3121f578175e85cdbfb2d
moctezumadaniel <moctezumadanielt@gmail.com>
2022-01-22 16:12:19 -0600

    auth0 credentials changed

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,21 @@
-import {Provider} from 'react-redux';
-import {createStore} from 'redux'
-import rootReducer from '../redux/reducers'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import MainTabButtons from '../components/Main_tab_buttons'
-import CustomerProfileTemplate from './Customer_profile_template'
-import AppMainContainer from '../components/App_main_container';
-import {Auth0Provider} from '@auth0/auth0-react'
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "../redux/reducers";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import MainTabButtons from "../components/Main_tab_buttons";
+import CustomerProfileTemplate from "./Customer_profile_template";
+import AppMainContainer from "../components/App_main_container";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const store = createStore (rootReducer)
+const store = createStore(rootReducer);
 export default function Home() {
   return (
     <Auth0Provider
-    domain="danielmoctezuma.us.auth0.com"
-    clientId="lIyLMie9a90fQfzV7kbLpt3xeR5HqbMo"
-    redirectUri={"http://localhost:3000/"}
-    audience="https://danielmoctezuma.us.auth0.com/api/v2/"
-    scope="read:current_user update:current_user_metadata">
+      domain="dev-006imb0h.us.auth0.com"
+      clientId="g6P2mkVcDFDJpEy1L8pguWxkDOUHuqiK"
+      redirectUri="http://localhost:3000/"
+    >
       <Provider store={store}>
         <div scroll="no" className={styles.container}>
           <Head>
@@ -24,10 +23,10 @@ export default function Home() {
             <link rel="icon" href="/celer.png" />
           </Head>
           {/*<CustomerProfileTemplate/>*/}
-          <AppMainContainer/>
-          <MainTabButtons/>
+          <AppMainContainer />
+          <MainTabButtons />
         </div>
       </Provider>
     </Auth0Provider>
-  )
+  );
 }
